2fef6afafc1695e3ff16b856568d8a140b636526
Olle Aid Kvarnström <olle+git@iix.se>
2021-11-02 19:51:17 +0100

    prettified the code

--- bomguess.go
+++ bomguess.go
@@ -1,16 +1,19 @@
 package main
 
+import "bytes"
+
 func GuessEncodingFromBOM(data *[]byte, dataSize int) Encoding {
     if dataSize < 2 || data == nil {
         return UNKNOWN
     }
 
-    if (*data)[0] == 0xFE && (*data)[1] == 0xFF {
+    var twoBytes = (*data)[:2]
+    if bytes.Equal(twoBytes, []byte {0xFE, 0xFF}) {
         return UTF16_BE
     }
 
-    if (*data)[0] == 0xFF && (*data)[1] == 0xFE {
-        if dataSize >= 4 && (*data)[2] == 0x00 && (*data)[3] == 0x00 {
+    if bytes.Equal(twoBytes, []byte {0xFF, 0xFE}) {
+        if dataSize >= 4 && bytes.Equal((*data)[2:4], []byte{0x00, 0x00}) {
             return UTF32_LE
         } else {
             return UTF16_LE
@@ -20,16 +23,17 @@ func GuessEncodingFromBOM(data *[]byte, dataSize int) Encoding {
     if dataSize < 3 {
         return UNKNOWN
     }
+    var threeBytes = (*data)[:3]
 
-    if (*data)[0] == 0xF7 && (*data)[1] == 0x64 && (*data)[2] == 0x4C {
+    if bytes.Equal(threeBytes, []byte {0xF7, 0x64, 0x4C}) {
         return UTF1
     }
 
-    if (*data)[0] == 0x2B && (*data)[1] == 0x2F && (*data)[2] == 0x76 {
+    if bytes.Equal(threeBytes, []byte {0x2B, 0x2F, 0x76}) {
         return UTF7
     }
 
-    if (*data)[0] == 0xEF && (*data)[1] == 0xBB && (*data)[2] == 0xBF {
+    if bytes.Equal(threeBytes, []byte {0xEF, 0xBB, 0xBF}) {
         return UTF8
     }
 
@@ -37,7 +41,8 @@ func GuessEncodingFromBOM(data *[]byte, dataSize int) Encoding {
         return UNKNOWN
     }
 
-    if (*data)[0] == 0x00 && (*data)[1] == 0x00 && (*data)[2] == 0xFE && (*data)[3] == 0xFF {
+    var fourBytes = (*data)[:4]
+    if bytes.Equal(fourBytes, []byte {0x00, 0x00, 0xFE, 0xFF}) {
         return UTF32_BE
     }
 
