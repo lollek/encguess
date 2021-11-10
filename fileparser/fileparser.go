7ba6e4384bc7cf49efb5fcf5567ad09b3cbc3919
Olle Aid Kvarnström <olle+git@iix.se>
2021-11-10 20:23:03 +0100

    Add flags
    Also put the file parsing logic in its own module

---
+++ fileparser/fileparser.go
@@ -0,0 +1,76 @@
+package fileparser
+
+import (
+	"fmt"
+	"github.com/lollek/encodingutil/checker"
+	"github.com/lollek/encodingutil/encoding"
+	"io"
+	"os"
+)
+
+type PotentialEncodingPair struct {
+	Encoding encoding.Encoding
+	Probability checker.Probability
+}
+
+type ParseFileResult struct {
+	PotentialEncodings []PotentialEncodingPair
+}
+
+func ParseFile(fileName string) (*ParseFileResult, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open file `%s`: %s\n", fileName, err)
+	}
+
+	data := make([]byte, os.Getpagesize())
+
+	checkers := []checker.EncodingChecker{
+		checker.NewBomChecker(),
+		checker.NewPrintableAsciiChecker(),
+		checker.NewAsciiChecker(),
+		checker.NewUtf8Checker(),
+		checker.NewIso88591Checker(),
+	}
+loop:
+	for {
+		count, err := file.Read(data)
+		switch err {
+		case io.EOF:
+			break loop
+		case nil:
+			break
+		default:
+			return nil, fmt.Errorf("Error reading from file `%s`: %s\n", fileName, err)
+		}
+
+		for i := 0; i < count; i++ {
+			character := data[i]
+			for checkerIndex := 0; checkerIndex < len(checkers); {
+				checker := checkers[checkerIndex]
+				checker.CheckNext(character)
+				if !checker.Validates() {
+					checkers = append(checkers[:checkerIndex], checkers[checkerIndex+1:]...)
+				} else {
+					checkerIndex++
+				}
+			}
+			if len(checkers) == 0 {
+				break loop
+			}
+		}
+	}
+
+	potentialEncodings := make([]PotentialEncodingPair, len(checkers))
+	for i, checker := range checkers {
+		potentialEncodings[i] = PotentialEncodingPair{
+			Encoding: checker.Encoding(),
+			Probability: checker.Probability(),
+		}
+	}
+
+	return &ParseFileResult{
+		PotentialEncodings: potentialEncodings,
+	}, nil
+}
+
--- main.go
+++ main.go
@@ -1,76 +1,59 @@
 package main
 
 import (
+	"flag"
+	"github.com/lollek/encodingutil/fileparser"
 	"fmt"
-	"github.com/lollek/encodingutil/checker"
-	"io"
 	"os"
+	"sort"
 )
 
-func printUsage(programName string) {
-	fmt.Fprintf(os.Stderr, "%s: FILENAME\n", programName)
+var (
+	printFileName *bool
+	printAllPotentialEncodings *bool
+)
+
+func printParseFileResult(fileName string, result *fileparser.ParseFileResult) {
+	results := result.PotentialEncodings
+	sort.Slice(results, func(a, b int) bool {
+		return results[a].Probability > results[b].Probability
+	})
+
+	if *printFileName {
+		fmt.Printf("%s: ", fileName)
+	}
+
+	numResults := len(results)
+	if numResults > 1 && !*printAllPotentialEncodings {
+		numResults = 1
+	}
+	switch numResults {
+	case 0:
+		fmt.Printf("Unknown\n")
+	case 1:
+		fmt.Printf("%s\n", results[0].Encoding)
+	default:
+		for i, guess := range results {
+			if i != 0 {
+				fmt.Printf(" or ")
+			}
+			fmt.Printf("%s", guess.Encoding)
+		}
+		println()
+	}
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		printUsage(os.Args[0])
-		os.Exit(1)
-	}
+	printFileName = flag.Bool("f", false, "Prefix output with the file name")
+	printAllPotentialEncodings = flag.Bool("a", false, "Print all potential encodings, instead of just the most likely. Will be in order of likelyness")
+	flag.Parse()
 
-	fileName := os.Args[1]
-	file, err := os.Open(fileName)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to open file `%s`: %s\n", fileName, err)
-		os.Exit(1)
-	}
-
-	data := make([]byte, os.Getpagesize())
-
-	checkers := []checker.EncodingChecker{
-		checker.NewBomChecker(),
-		checker.NewPrintableAsciiChecker(),
-		checker.NewAsciiChecker(),
-		checker.NewUtf8Checker(),
-		checker.NewIso88591Checker(),
-	}
-loop:
-	for {
-		count, err := file.Read(data)
-		switch err {
-		case io.EOF:
-			break loop
-		case nil:
-			break
-		default:
-			fmt.Fprintf(os.Stderr, "Error reading from file `%s`: %s\n", fileName, err)
-			os.Exit(1)
-		}
-
-		for i := 0; i < count; i++ {
-			character := data[i]
-			for checkerIndex := 0; checkerIndex < len(checkers); {
-				checker := checkers[checkerIndex]
-				checker.CheckNext(character)
-				if !checker.Validates() {
-					checkers = append(checkers[:checkerIndex], checkers[checkerIndex+1:]...)
-				} else {
-					checkerIndex++
-				}
-			}
-			if len(checkers) == 0 {
-				break loop
-			}
-		}
-	}
-
-	switch len(checkers) {
-	case 0:
-		println("Unknown")
-	case 1:
-		fmt.Printf("%s (probability: %s)\n", checkers[0].Encoding(), checkers[0].Probability())
-	default:
-		for _, checker := range checkers {
-			fmt.Printf("%s (probability: %s)\n", checker.Encoding(), checker.Probability())
+	for _, fileName := range flag.Args() {
+		result, err := fileparser.ParseFile(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s", err)
+		} else {
+			printParseFileResult(fileName, result)
 		}
 	}
 }
